0e391e51c2cabbd574fcb86a36938e5ad860e29d
yates222 <yateszhou22@gmail.com>
2021-12-08 21:29:06 +0800

    feature: add strategy pattern

--- design_pattern/strategy.py
+++ design_pattern/strategy.py
@@ -7,4 +7,60 @@
 缺点： 策略类会增多
 """
 
+
 # 解决if-else过多的问题
+# 假设小明要从A城市到B城市
+class Strategy:
+    def move(self):
+        pass
+
+
+class CarStrategy(Strategy):
+    def move(self):
+        print("乘坐了汽车。")
+
+
+class PlaneStrategy(Strategy):
+    def move(self):
+        print("乘坐了飞机。")
+
+
+class TrainStrategy(Strategy):
+    def move(self):
+        print("乘坐了火车。")
+
+
+class Person:
+
+    def __init__(self, strategy):
+        self.strategy = strategy
+
+    def execute(self):
+        self.strategy.move()
+
+
+# 一般写法
+def move(strategy):
+    if strategy == "car":
+        print("乘坐了汽车")
+    elif strategy == "plane":
+        print("乘坐了飞机")
+    elif strategy == "train":
+        print("乘坐了火车")
+
+
+"""
+一般写法需要向函数传递一个类型参数，具体的分支判断与执行逻辑都在
+函数中，不易扩展。
+策略模式只需要向Person中传递一个Strategy对象，而Strategy对象是可以
+自己创建的，方便扩展。
+"""
+if __name__ == "__main__":
+    xiaoming = Person(CarStrategy())
+    xiaoming.execute()
+
+    xiaoming = Person(PlaneStrategy())
+    xiaoming.execute()
+
+    xiaoming = Person(TrainStrategy())
+    xiaoming.execute()
